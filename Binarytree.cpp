287423067d2b956e919e149464c3a1090a9ddee3
AlifKurnia14 <tsimpsons056@gmail.com>
2025-06-26 16:04:00 +0700

    step 7: if the value in the data field of the new node is greatre than that of the parent

--- Binarytree.cpp
+++ Binarytree.cpp
@@ -75,4 +75,14 @@ class BinaryTree{
 
         }
 
-        
+        // step 7: if the value in the data field of the new node is greatre than that of the parent
+        else if (x >parent ->info){
+            //7a :make the right child of the parent point to the new node
+            parent ->rightchild=newNode;
+
+            //7b : exit
+            return;
+        }
+    }
+
+    
